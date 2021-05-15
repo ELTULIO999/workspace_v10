5050c44fb127d1aa9c5944ab07b7a77c115cf7f5
ELTULIO999 <69618767+ELTULIO999@users.noreply.github.com>
2021-05-15 02:38:04 -0600

    GG eZ

--- project2_digy2/main.c
+++ project2_digy2/main.c
@@ -35,61 +35,100 @@ __error__(char *pcFilename, uint32_t ui32Line)
 // Blink the on-board LED.
 //
 //*****************************************************************************
-
 uint8_t flag;
+
 int
-main(void)
-{
+main(void){
 
     volatile uint32_t ui32Loop;
+
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){
-    }
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){ }
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)){ }
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)){ }
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)){ }
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)){ }
+
+
+
+    //INPUTS
+    GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_5, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPadConfigSet(GPIO_PORTE_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    //LEDS_Green
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0);
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_2);
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_1);
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_3);
+    //red
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_1);
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_2);
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_7);
+
+    //com_esp
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_5);
+    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_4);
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_5);
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_6);
+
+
+
+
     // Loop forever.
-    while(1)
-    {
-        if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)==0){
-                    flag=1;}
-                else {
-                    if(flag==1){
-                        //green
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
-                        SysCtlDelay(2000000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-                        SysCtlDelay(2000000);
+    while(1){
+       if (GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_5)==0){
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, GPIO_PIN_0);
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0);
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_5, GPIO_PIN_5);
 
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
-                        SysCtlDelay(2000000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-                        SysCtlDelay(2000000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
-                        SysCtlDelay(2000000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-                        SysCtlDelay(200000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
-                        SysCtlDelay(2000000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-                        SysCtlDelay(200000);
-                        //yel
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
-                        SysCtlDelay(2000000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
-                        SysCtlDelay(2000000);
-                        //red
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
-                        SysCtlDelay(2000000);
-                        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
-                        SysCtlDelay(2000000);
-                        flag=0;
-                        }
-                    }
+       }
+        else{
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, GPIO_PIN_1);
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, 0);
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_5, 0);
+        }
+
+       if (GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_0)==0){
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2,GPIO_PIN_2);
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 0);
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_PIN_4);
+       }
+        else{
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, GPIO_PIN_3);
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0);
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, 0);
+        }
+
+        if (GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_1)==0){
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_PIN_1);
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0);
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_PIN_5);
+        }
+        else{
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, GPIO_PIN_2);
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 0);
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, 0);
+        }
+
+        if (GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_4)==0){
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, GPIO_PIN_3);
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, 0);
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_PIN_6);
+
+        }
+        else{
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, GPIO_PIN_7);
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0);
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, 0);
+        }
 
     }
-
 }
